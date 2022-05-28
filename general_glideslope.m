d9a18c57e0d638bbb4f858262f05aa4b93fa84d2
James Le <le_james@outlook.com>
2022-05-28 11:28:28 -0700

    guidance trajectory for general rendezvous

---
+++ general_glideslope.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = general_glideslope(r0,mission_targets)
+% general_glideslope: generates an inbound glideslope guidance trajectory
+%   input(s): mission_targets - 7x1xnumber_of_targets matrix containing the
+%                               mission (look at example on how to setup)
+
+
+end
